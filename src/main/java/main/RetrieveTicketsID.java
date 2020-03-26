39d82f7528f0afc9d2b887a42de48da8f939c409
Roberto Capannelli <roberto.capannelli@gmail.com>
2020-03-26 09:27:33 +0100

    Fix: code style improvements

--- src/main/java/main/RetrieveTicketsID.java
+++ src/main/java/main/RetrieveTicketsID.java
@@ -8,6 +8,56 @@ import java.net.URL;
 import java.nio.charset.Charset;
 
 public class RetrieveTicketsID {
+
+    public static void main(String[] args) throws IOException {
+        int maxResults = 0;
+        int startAt = 0;
+        int issuesCount = 1;
+        int upperBound = 1000;;
+
+        do {
+            maxResults = startAt + upperBound; //Only gets a max of 1000 at a time, so must do this multiple times if bugs >1000
+
+            JSONObject json = readJSONFromUrl(startAt, maxResults);
+            JSONArray issues = json.getJSONArray("issues");
+            issuesCount = json.getInt("total");
+
+            for (; startAt < issuesCount && startAt < maxResults; startAt++) {
+                String key = issues.getJSONObject(startAt % upperBound).get("key").toString();
+                System.out.println(key);
+            }
+        } while (startAt < issuesCount);
+    }
+
+    private static JSONObject readJSONFromUrl(Integer startAt, Integer maxResults) throws IOException {
+        String url = generateJiraUrl(startAt, maxResults);
+        InputStream inputStream = new URL(url).openStream();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+            String jsonText = readAll(bufferedReader);
+            return new JSONObject(jsonText);
+        } finally {
+            inputStream.close();
+        }
+    }
+
+    private static String generateJiraUrl(Integer startAt, Integer maxResults) {
+        String projectName = "QPID";
+        return "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
+                + projectName + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
+                + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
+                + startAt.toString() + "&maxResults=" + maxResults.toString();
+    }
+
+    private static String readAll(Reader bufferedReader) throws IOException {
+        int charPointer;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((charPointer = bufferedReader.read()) != -1) {
+            stringBuilder.append((char) charPointer);
+        }
+        return stringBuilder.toString();
+    }
+
     /*public static JSONArray readJsonArrayFromUrl(String url) throws IOException {
         InputStream inputStream = new URL(url).openStream();
         try {
@@ -20,60 +70,9 @@ public class RetrieveTicketsID {
             inputStream.close();
         }
     }*/
-
-    private static String readAll(Reader bufferedReader) throws IOException {
-        int charPointer;
-        StringBuilder stringBuilder = new StringBuilder();
-        while ((charPointer = bufferedReader.read()) != -1) {
-            stringBuilder.append((char) charPointer);
-        }
-        return stringBuilder.toString();
-    }
-
-    private static JSONObject readJSONFromUrl(String url) throws IOException {
-        InputStream inputStream = new URL(url).openStream();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-            String jsonText = readAll(bufferedReader);
-            return new JSONObject(jsonText);
-        } finally {
-            inputStream.close();
-        }
-    }
-
-    private static String generateJiraUrl(String projectName, Integer startAt, Integer maxResults) {
-        return "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
-                + projectName + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
-                + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
-                + startAt.toString() + "&maxResults=" + maxResults.toString();
-    }
-
-    public static void main(String[] args) throws IOException {
-        int maxResults = 0;
-        int startAt = 0;
-        int issuesCount = 1;
-        int upperBound = 1000;
-        String totalKey = "total";
-        String issuesKey = "issues";
-        String issueKey = "key";
-        String projectName = "QPID";
-
-        do {
-            maxResults = startAt + upperBound; //Only gets a max of 1000 at a time, so must do this multiple times if bugs >1000
-            String url = generateJiraUrl(projectName, startAt, maxResults);
-
-            JSONObject json = readJSONFromUrl(url);
-            issuesCount = json.getInt(totalKey);
-
-            JSONArray issues = json.getJSONArray(issuesKey);
-
-            for (; startAt < issuesCount && startAt < maxResults; startAt++) {
-                //Iterate through each bug
-                String key = issues.getJSONObject(startAt % upperBound).get(issueKey).toString();
-                System.out.println(key);
-            }
-        } while (startAt < issuesCount);
-
-    }
 }
+
+//TODO see apis
+// @link https://issues.apache.org/jira/rest/api/2/search?jql=project=%22QPID%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt=0&maxResults=12
+
 
